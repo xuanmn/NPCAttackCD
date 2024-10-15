b3a723355f06a5555fd17104084b11732021a966
Xuan Nguyen <nguyemoc000@gmail.com>
2024-10-15 01:33:11 -0700

    struc improvements 3

--- src/test/java/com/ezInferno/ezInfernoPluginTest.java
+++ src/test/java/com/ezInferno/ezInfernoPluginTest.java
@@ -1,6 +1,6 @@
 package com.ezInferno;
 
-import ezInferno.ezInfernoPlugin;
+import com.ezInferno.ezInfernoPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
